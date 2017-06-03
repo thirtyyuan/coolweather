67cbfd6a0210a723854ed79873fc5926435c2d89
thirtyyuan <thirtyyuan@gmail.com>
2017-06-03 20:32:00 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,44 @@
+package com.coolweather.android.db;
+
+/**
+ * Created by yuanhonglei on 8/2/16.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getweatherId() {
+        return weatherId;
+    }
+
+    public void setweatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
